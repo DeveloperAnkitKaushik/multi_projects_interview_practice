76368689af5cc8f45e9f5b007a8279353e5afb74
Ankit Kaushik <ankitkaushik6269@gmail.com>
2024-08-13 14:16:15 +0530

    1 day

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Accordian from "./component/accordian/Accordian"
+
+const App = () => {
+  return (
+    <div className="appcontainer">
+      <Accordian/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/accordian copy/Accordian.jsx
@@ -0,0 +1,11 @@
+import styles from './index.module.css';
+
+const Accordian = () => {
+  return (
+    <div className={styles.container}>
+        
+    </div>
+  )
+}
+
+export default Accordian
